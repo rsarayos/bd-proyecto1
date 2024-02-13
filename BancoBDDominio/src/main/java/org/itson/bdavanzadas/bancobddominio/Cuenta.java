bbcbe39d5d2e44e96f9c403abe0107fb8af61af9
rsarayos <raul.sauceda@potros.itson.edu.mx>
2024-02-13 16:49:35 -0700

    act
    act

--- BancoBDDominio/src/main/java/org/itson/bdavanzadas/bancobddominio/Cuenta.java
+++ BancoBDDominio/src/main/java/org/itson/bdavanzadas/bancobddominio/Cuenta.java
@@ -5,6 +5,7 @@
 package org.itson.bdavanzadas.bancobddominio;
 
 import java.util.Date;
+import java.util.Objects;
 
 /**
  *
@@ -16,7 +17,91 @@ public class Cuenta {
     private long saldo;
     private boolean estado;
     private String telefonoTitular;
-    
-    
+
+    public Cuenta() {
+    }
+
+    public Cuenta(String numCuenta, Date fechaApertura, long saldo, String telefonoTitular) {
+        this.numCuenta = numCuenta;
+        this.fechaApertura = fechaApertura;
+        this.saldo = saldo;
+        this.estado = true;
+        this.telefonoTitular = telefonoTitular;
+    }
+
+    public String getNumCuenta() {
+        return numCuenta;
+    }
+
+    public void setNumCuenta(String numCuenta) {
+        this.numCuenta = numCuenta;
+    }
+
+    public Date getFechaApertura() {
+        return fechaApertura;
+    }
+
+    public void setFechaApertura(Date fechaApertura) {
+        this.fechaApertura = fechaApertura;
+    }
+
+    public long getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(long saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getTelefonoTitular() {
+        return telefonoTitular;
+    }
+
+    public void setTelefonoTitular(String telefonoTitular) {
+        this.telefonoTitular = telefonoTitular;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.numCuenta);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cuenta other = (Cuenta) obj;
+        return Objects.equals(this.numCuenta, other.numCuenta);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cuenta{");
+        sb.append("numCuenta=").append(numCuenta);
+        sb.append(", fechaApertura=").append(fechaApertura);
+        sb.append(", saldo=").append(saldo);
+        sb.append(", estado=").append(estado);
+        sb.append(", telefonoTitular=").append(telefonoTitular);
+        sb.append('}');
+        return sb.toString();
+    }
     
 }
