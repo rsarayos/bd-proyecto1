7b0d548373db373ac2489e173437664227181e75
rsarayos <raul.sauceda@potros.itson.edu.mx>
2024-02-13 16:32:13 -0700

    inicio
    inicio

---
+++ BancoBD/src/main/java/org/itson/bdavanzadas/bancobd/BancoBD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.bdavanzadas.bancobd;
+
+/**
+ *
+ * @author alex_
+ */
+public class BancoBD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
