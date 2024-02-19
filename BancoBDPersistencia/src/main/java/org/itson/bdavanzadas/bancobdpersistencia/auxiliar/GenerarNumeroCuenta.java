d342b022a189ad7cdb2605c88039a070b181a06b
rsarayos <raul.sauceda@potros.itson.edu.mx>
2024-02-19 12:42:58 -0700

    Persistencia
    Documentacion paquete auxiliares

--- BancoBDPersistencia/src/main/java/org/itson/bdavanzadas/bancobdpersistencia/auxiliar/GenerarNumeroCuenta.java
+++ BancoBDPersistencia/src/main/java/org/itson/bdavanzadas/bancobdpersistencia/auxiliar/GenerarNumeroCuenta.java
@@ -4,25 +4,38 @@ package org.itson.bdavanzadas.bancobdpersistencia.auxiliar;
 import java.util.Random;
 
 /**
- *
- * @author alex_
+ * La clase GenerarNumeroCuenta proporciona un método para generar números de cuenta aleatorios
+ * en un formato específico.
+ * 
+ * @author Victor Humberto Encinas Guzman & Raul Alejandro Sauceda Rayos
  */
 public class GenerarNumeroCuenta {
-    
+
+    /**
+     * Genera un número de cuenta aleatorio con un formato específico que
+     * consiste en los primeros tres dígitos fijos (580) seguidos por tres
+     * dígitos aleatorios generados.
+     *
+     * @return Número de cuenta aleatorio generado.
+     */
     public String generarNumeroCuenta() {
         String primerosTresDigitos = "580";
         String ultimosTresDigitos = generarDigitosAleatorios();
         String numeroCuenta = primerosTresDigitos + ultimosTresDigitos;
-
         return numeroCuenta;
     }
 
+    /**
+     * Genera una cadena de tres dígitos aleatorios.
+     *
+     * @return Tres dígitos aleatorios en formato de cadena.
+     */
     private String generarDigitosAleatorios() {
         Random random = new Random();
         int numeroAleatorio = random.nextInt(1000);
+        // Formatea el número aleatorio a una cadena de tres dígitos rellenados con ceros a la izquierda si es necesario.
         String digitosAleatorios = String.format("%03d", numeroAleatorio);
-
         return digitosAleatorios;
     }
-    
+
 }
--- BancoBDPersistencia/src/main/java/org/itson/bdavanzadas/bancobdpersistencia/auxiliar/Validaciones.java
+++ BancoBDPersistencia/src/main/java/org/itson/bdavanzadas/bancobdpersistencia/auxiliar/Validaciones.java
@@ -8,129 +8,191 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
+ * La clase Validaciones proporciona métodos para validar diferentes tipos de datos, como nombres,
+ * apellidos, teléfonos, fechas, domicilios, números, códigos postales, cuentas, contraseñas y cantidades.
  *
- * @author alex_
+ * @author Victor Humberto Encinas Guzman & Raul Alejandro Sauceda Rayos
  */
 public class Validaciones {
-    
+
+    /**
+     * Valida si el nombre proporcionado cumple con el formato permitido.
+     *
+     * @param nombre Nombre a validar.
+     * @return true si el nombre es válido, false de lo contrario.
+     */
     public boolean validaNombre(String nombre) {
 
         Pattern patron = Pattern.compile("^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\\s]{1,100}$");
         Matcher matcher = patron.matcher(nombre);
-        
+
         if (matcher.matches()) {
             return true;
         } else {
             return false;
         }
     }
-    
+
+    /**
+     * Valida si el apellido proporcionado cumple con el formato permitido.
+     *
+     * @param apellido Apellido a validar.
+     * @return true si el apellido es válido, false de lo contrario.
+     */
     public boolean validaApellidos(String apellido) {
 
         Pattern patron = Pattern.compile("^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ]{1,50}$");
         Matcher matcher = patron.matcher(apellido);
-        
+
         if (matcher.matches()) {
             return true;
         } else {
             return false;
         }
     }
-    
+
+    /**
+     * Valida si el número de teléfono proporcionado cumple con el formato permitido.
+     *
+     * @param telefono Número de teléfono a validar.
+     * @return true si el número de teléfono es válido, false de lo contrario.
+     */
     public boolean validaTelefono(String telefono) {
 
         Pattern patron = Pattern.compile("^\\d{10}$");
         Matcher matcher = patron.matcher(telefono);
-        
+
         if (matcher.matches()) {
             return true;
         } else {
             return false;
         }
     }
-    
+
+    /**
+     * Valida si la fecha proporcionada cumple con el formato permitido.
+     *
+     * @param fecha Fecha a validar.
+     * @return true si la fecha es válida, false de lo contrario.
+     */
     public boolean validaFecha(String fecha) {
 
         Pattern patron = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}$");
         Matcher matcher = patron.matcher(fecha);
-        
+
         if (matcher.matches()) {
             return true;
         } else {
             return false;
         }
     }
-    
+
+    /**
+     * Valida si el domicilio proporcionado cumple con el formato permitido.
+     *
+     * @param domicilio Domicilio a validar.
+     * @return true si el domicilio es válido, false de lo contrario.
+     */
     public boolean validaDomicilio(String domicilio) {
 
         Pattern patron = Pattern.compile("^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\\d\\s]{1,50}$");
         Matcher matcher = patron.matcher(domicilio);
-        
+
         if (matcher.matches()) {
             return true;
         } else {
             return false;
         }
     }
-    
+
+    /**
+     * Valida si el número proporcionado cumple con el formato permitido.
+     *
+     * @param numero Número a validar.
+     * @return true si el número es válido, false de lo contrario.
+     */
     public boolean validaNumero(String numero) {
 
         Pattern patron = Pattern.compile("^\\d{1,10}$");
         Matcher matcher = patron.matcher(numero);
-        
+
         if (matcher.matches()) {
             return true;
         } else {
             return false;
         }
     }
-    
+
+    /**
+     * Valida si el código postal proporcionado cumple con el formato permitido.
+     *
+     * @param cp Código postal a validar.
+     * @return true si el código postal es válido, false de lo contrario.
+     */
     public boolean validaCp(String cp) {
 
         Pattern patron = Pattern.compile("^\\d{1,5}$");
         Matcher matcher = patron.matcher(cp);
-        
+
         if (matcher.matches()) {
             return true;
         } else {
             return false;
         }
     }
-    
+
+    /**
+     * Valida si el número de cuenta proporcionado cumple con el formato permitido.
+     *
+     * @param cuenta Número de cuenta a validar.
+     * @return true si el número de cuenta es válido, false de lo contrario.
+     */
     public boolean validaCuenta(String cuenta) {
 
         Pattern patron = Pattern.compile("^\\d{6}$");
         Matcher matcher = patron.matcher(cuenta);
-        
+
         if (matcher.matches()) {
             return true;
         } else {
             return false;
         }
     }
-    
+
+    /**
+     * Valida si la contraseña proporcionada cumple con el formato permitido.
+     *
+     * @param contrasenia Contraseña a validar.
+     * @return true si la contraseña es válida, false de lo contrario.
+     */
     public boolean validaContra(String contrasenia) {
 
         Pattern patron = Pattern.compile("^[a-zA-Z0-9.,\\-_\\*]{1,20}$");
         Matcher matcher = patron.matcher(contrasenia);
-        
+
         if (matcher.matches()) {
             return true;
         } else {
             return false;
         }
     }
-    
+
+    /**
+     * Valida si la cantidad proporcionada cumple con el formato permitido.
+     *
+     * @param cantidad Cantidad a validar.
+     * @return true si la cantidad es válida, false de lo contrario.
+     */
     public boolean validaCantidad(String cantidad) {
 
         Pattern patron = Pattern.compile("^(?!0\\d)(\\d+(\\.\\d{1,2})?)?$");
         Matcher matcher = patron.matcher(cantidad);
-        
+
         if (matcher.matches()) {
             return true;
         } else {
             return false;
         }
     }
-    
+
 }
